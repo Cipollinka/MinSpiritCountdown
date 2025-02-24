e7129046c5eeb5a558c269872f9374563ebd4b30
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-02-24 23:12:46 +0200

    Update Params.js

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = '0c643c2d-b4f6-4516-bee9-bf4b40401678'; // ключ OneSignal
   static appID = '6740518975'; // ID додатку в сторі
   static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
-  static targetDate = '2025-02-20'; // дата закінчення відльожки
+  static targetDate = '2025-02-26'; // дата закінчення відльожки
   static bodyLin = 'https://remarkable-royal-jubilation.space/dpgbt4Qh'; // тіло лінки з якою будемо працювати
 }
